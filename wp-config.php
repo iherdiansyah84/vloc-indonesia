be751724167a5274dd40105177dd478ec7560f42
ARKA-IPAN\iherdiansyah <ipan.herdiansyah@arkamaya.co.id>
2017-11-17 08:49:59 +0700

    update

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'vloc');
+define('DB_NAME', 'u724189029_vloc');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'u724189029_root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'satu2tiga');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.hostinger.co.id');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
